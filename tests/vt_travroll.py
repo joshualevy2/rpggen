195e98bbe38e62afac7d872e1de416df9a15cb4d
Joshua levy <levyj@vmware.com>
2017-06-08 17:07:26 -0700

    better

--- tests/vt_travroll.py
+++ tests/vt_travroll.py
@@ -10,8 +10,13 @@ def testroll(dicestr=None, target=None):
          results.append(str(Traveller.roll(target)))
       else:
          results.append(str(Traveller.roll(dicestr,target)))
+   if dicestr is None:
+      dicestr = 'Traveller.default'      
    print('%s target=%s: %s' % (str(dicestr), target, " ".join(results)))
  
 # execute only if run as a script    
 if __name__ == "__main__":  
     testroll(target='5+')
+    testroll(target='5-')
+    testroll(target='11+')
+    testroll(target='11-')
