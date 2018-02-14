9f8d6123629e8fc869791d864be9be7c2e8608e4
Joshua Levy <joshualevy2@hotmail.com>
2018-02-13 17:47:55 -0800

    tests for blank lines in lt files

--- tests/t_tableLt.py
+++ tests/t_tableLt.py
@@ -13,27 +13,29 @@ class TestTableLt(unittest.TestCase):
       Rpggen.testData = 2
       #logging.basicConfig(level=logging.DEBUG)
 
+   def twicetest(self,table,result,entries):
+      res1 = table.use()
+      self.assertEqual(res1,result)
+      res2 = table.use()
+      self.assertEqual(res2,result)
+      values = table.results()
+      self.assertEqual(len(values),entries) 
+
    def test_basic(self):
       tab = Rpggen.loadLt('../samples/PersonalityTraits.lt')
-      res1 = tab.use()
-      self.assertEqual(res1,'Abrasive')
-      res2 = tab.use()
-      self.assertEqual(res2,'Abrasive')
-      values = tab.results()
-      #values.sort()
-      self.assertEqual(len(values),400)  
+      self.twicetest(tab,'Abrasive',399)
 
    def test_removeFirstToken(self):
       tab = Rpggen.loadLt('../samples/PersonalityTraits100.lt')
-      res1 = tab.use()
-      print(res1)
-      self.assertEqual(res1,'Always Betray Rebel Scum')
-      res2 = tab.use()
-      print(res2)
-      self.assertEqual(res2,'Always Betray Rebel Scum')
-      values = tab.results()
-      #values.sort()
-      self.assertEqual(len(values),100) 
+      self.twicetest(tab,'Always Betray Rebel Scum',100)   
+
+   def test_blank1(self):
+      tab = Rpggen.loadLt('lt-files/blank1.lt')
+      self.twicetest(tab,'two',2)    
+
+   def test_blank2(self):
+      tab = Rpggen.loadLt('lt-files/blank2.lt')
+      self.twicetest(tab,'two',2)   
 
 if __name__ == '__main__':  
    unittest.main()
